c1ac19a4e897981d27e26bf2fcce240664c19165
lmn451 <putaluta@tuta.io>
2026-01-03 18:50:28 +0200

    chore: add framework examples

---
+++ example/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container" style="color: var(--vue-color); background: var(--vue-bg);">
+    Vue example
+  </div>
+</template>
+
+<style>
+:root {
+  --vue-color: #e11d48;
+  --vue-bg: #ffe4e6;
+}
+
+.container {
+  padding: 1rem;
+  border: 1px solid var(--vue-color);
+}
+</style>
